4a4dae18a0d571e96f29b097a5ecfbf4abc1148b
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2023-07-19 01:36:29 +0900

    整数リテラルと足し算を実装

---
+++ libminruby.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int print_int(int x) {
+    printf("%d\n", x);
+    return x;
+}
---
+++ minrubyc.rb
@@ -0,0 +1,50 @@
+require "minruby"
+
+def gen(tree)
+  if tree[0] == "lit"
+    puts "\tmov w0, ##{tree[1]}"
+  elsif tree[0] == "+"
+    arg1 = tree[1]
+    arg2 = tree[2]
+
+    # スタックを確保
+    puts "\tsub sp, sp, #32"
+
+    # arg1 を評価
+    gen(arg1)
+    puts "\tstr w0, [sp, #16]"
+
+    # arg2 を評価
+    gen(arg2)
+    puts "\tstr w0, [sp, #0]"
+
+    # arg1 + arg2 を計算
+    puts "\tldr w8, [sp, #16]"
+    puts "\tldr w9, [sp, #0]"
+    puts "\tadd w0, w8, w9"
+
+    # スタックを解放
+    puts "\tadd sp, sp, #32"
+  else
+    raise "invalid AST: #{tree}"
+  end
+end
+
+tree = minruby_parse(ARGV[0])
+
+puts "\t.text"
+puts "\t.align 2"
+puts "\t.globl _main"
+puts "_main:"
+puts "\tsub sp, sp, #16"
+puts "\tstp x29, x30, [sp, #0]"
+
+gen(tree)
+
+# 入力した整数をプリントする
+puts "\tbl _print_int"
+
+puts "\tmov w0, #0"
+puts "\tldp x29, x30, [sp, #0]"
+puts "\tadd sp, sp, #16"
+puts "\tret"
